a0475e48bab9cff1b0c7faa236fef9d7362affa0
tsutaj <y.sugie.15739d@gmail.com>
2020-09-23 09:56:01 +0900

    add docs mode

--- src/manager/docs_manager.py
+++ src/manager/docs_manager.py
@@ -1,9 +1,41 @@
+import pathlib
+import pickle
 from src.manager.base_manager import BaseManager
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 class DocsManager(BaseManager):
     def __init__(self, project):
         super().__init__(project)
+        self.creds = None
+        
+        # set credentials
+        docs_setting = project.get_attr("docs")
+        token_path = pathlib.Path(docs_setting.get("token_src", ""))
+        creds_path = pathlib.Path(docs_setting.get("credentials_src", ""))
+        if token_path.exists():
+            with open(token_path, "rb") as token:
+                self.creds = pickle.load(token)
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(token_path, "wb") as token:
+                pickle.dump(self.creds, token)
 
-    def get_contents(self, task_name):
-        with open(hoge) as fuga:
-            pass
+        # launch a service
+        self.service = build("docs", "v1", credentials=self.creds)
+
+    def get_contents(self, statement_src: pathlib.Path) -> str:
+        document = self.service.documents().get(documentId=statement_src).execute()
+        text = ""
+        for content in document.get("body")["content"]:
+            if not "paragraph" in content:
+                continue
+            for element in content["paragraph"]["elements"]:                
+                text += element["textRun"]["content"]
+        return text
--- src/manager/local_manager.py
+++ src/manager/local_manager.py
@@ -6,6 +6,6 @@ class LocalManager(BaseManager):
     def __init__(self, project):
         super().__init__(project)
 
-    def get_contents(self, statement_src: pathlib.Path) -> List[str]:
+    def get_contents(self, statement_src: pathlib.Path) -> str:
         with open(statement_src) as f:
             return f.read()
