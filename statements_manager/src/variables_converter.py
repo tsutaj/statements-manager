70290abe69a5c3cfd8a87212e018441cbc940ecd
tsutaj <y.sugie.15739d@gmail.com>
2022-06-17 01:20:08 +0900

    #77 fix: to_string

--- statements_manager/src/variables_converter.py
+++ statements_manager/src/variables_converter.py
@@ -14,6 +14,8 @@ def to_string(value: Any) -> str:
             k = math.floor(math.log10(abs(value)))
             if value == 10**k:
                 return f"10^{{{k}}}"
+            elif value == -(10**k):
+                return f"-10^{{{k}}}"
             elif value % (10**k) == 0:
                 return f"{value // 10 ** k} \\times 10^{{{k}}}"
             else:
