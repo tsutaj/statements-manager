81168b40e5a32db2c365afa55618c08726b38332
tsutaj <y.sugie.15739d@gmail.com>
2023-06-22 02:32:16 +0900

    version management

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
+import os
+
 from setuptools import find_packages, setup
+
+
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="statements-manager",
-    version="1.7.13",
+    version=get_version("statements_manager/__init__.py"),
     author="Yuya Sugie",
     author_email="y.sugie.15739d@gmail.com",
     url="https://github.com/tsutaj/statements-manager",
